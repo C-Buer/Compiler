401b247d5e82df5419e6d2f25dc42c02db169847
Tab Ye <tabye320@outlook.com>
2024-10-16 21:35:28 +0800

    detect repo

--- xmake.lua
+++ xmake.lua
@@ -2,8 +2,16 @@ set_policy("package.install_always", true)
 add_rules("mode.release", "mode.debug")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = "./"})
 
+option("tab_repo")
+    set_default(true)
+    set_showmenu(true)
+option_end()
+
 add_languages("cxx20")
-add_repositories("tab-repo https://github.com/TabNahida/repo-xmake.git")
+
+if has_config("tab_repo") then
+    add_repositories("tab-repo https://github.com/TabNahida/repo-xmake.git")
+end
 add_requires("tstring")
 
 target("Lexer")

dcf15080f04c283849c65399bb4ea7bdad0472d0
Tab Ye <tabye320@outlook.com>
2024-10-27 14:29:52 +0800

    better config

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,5 @@
 set_policy("package.install_always", true)
 add_rules("mode.release", "mode.debug")
-add_rules("plugin.compile_commands.autoupdate", {outputdir = "./"})
 
 option("tab_repo")
     set_default(true)
@@ -9,7 +8,7 @@ option_end()
 
 add_languages("cxx20")
 
-if has_config("tab_repo") then
+if not has_config("tab_repo") then
     add_repositories("tab-repo https://github.com/TabNahida/repo-xmake.git")
 end
 add_requires("tstring")

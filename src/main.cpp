1115334bce25249410711017f8a9a85aac05d0d2
Tab Ye <tabye320@outlook.com>
2024-11-01 17:22:02 +0800

    format

--- src/main.cpp
+++ src/main.cpp
@@ -49,33 +49,7 @@ int main()
 
     for (const auto &token : tokens)
     {
-        std::cout << "Type: ";
-        switch (token.type)
-        {
-        case TokenType::Identifier:
-            std::cout << "Identifier";
-            break;
-        case TokenType::Keyword:
-            std::cout << "Keyword";
-            break;
-        case TokenType::Number:
-            std::cout << "Number";
-            break;
-        case TokenType::String:
-            std::cout << "String";
-            break;
-        case TokenType::Operator:
-            std::cout << "Operator";
-            break;
-        case TokenType::EndOfFile:
-            std::cout << "EndOfFile";
-            break;
-        case TokenType::Unknown:
-            std::cout << "Unknown";
-            break;
-        }
-        std::cout << ", Lexeme: \"" << token.lexeme << "\""
-                  << ", Line: " << token.line << ", Column: " << token.column << "\n";
+        std::cout << token.printToken() << std::endl;
     }
 
     return 0;
